a539a9b69d09dfee2d5fda481523b9b3bcec9579
Amado Tejada <tejada.amado@gmail.com>
2022-03-19 14:43:04 -0400

    Create testflight-beta-watcher.py

---
+++ testflight-beta-watcher.py
@@ -0,0 +1,45 @@
+import os
+
+import requests
+
+home = os.path.expanduser("~")
+url = "https://testflight.apple.com/join/"
+apps = {
+    "Slack": "QE3kgqJ2",
+    "Discord": "gdE4pRzI",
+    "ProtonMail": "8SxXknzD",
+    "1Password": "8SxXknzD",
+    "GitHub": "8SxXknzD",
+}
+
+
+def check_beta():
+    for app, code in apps.items():
+        tmpfile = f"{home}/.{app}"
+        print(f"Checking: {app} beta")
+        resp = requests.get(f"{url}/{code}")
+        if resp.ok:
+            if "beta is full" not in resp.text:
+                if not os.path.exists(tmpfile):
+                    send_push_alert(app, code, url)
+                    open(tmpfile, "a").close()
+            else:
+                if os.path.exists(tmpfile):
+                    os.remove(tmpfile)
+        else:
+            print(f"\n{app} not found, {resp.status_code}")
+
+
+def send_push_alert(app, code, url):
+    resp = requests.post(
+        "https://api.pushover.net/1/messages.json",
+        data={
+            "token": "{}",
+            "user": "{}",
+            "message": f"{app} Beta is available!\n{url+code}",
+        },
+    )
+    print(resp.text)
+
+
+check_beta()
